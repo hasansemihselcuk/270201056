cdb9891aa4ceb8a0b2c9c9758d01f625c643d0de
n72537645 <hss.2277@hotmail.com>
2020-11-23 08:55:47 +0000

    17.11.2020

---
+++ quiz/quiz6.py
@@ -0,0 +1,11 @@
+name = input("What is your name: ")
+for i in range(len(name)):
+  print(name[i].upper())
+x=("")
+mylist= []
+while x !="quit": 
+  mylist.append(x)
+  x=input("write quit? ")
+  
+
+print(mylist,"you are out")
